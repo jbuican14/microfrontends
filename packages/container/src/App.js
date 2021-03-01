ef502590548825f39bd8895cba48e153fb0667d4
Juti Buican <jbuican19@gmail.com>
2021-03-01 13:22:49 +0000

    Set prefix class name for production

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 
+// Set up the prefix for naming class
+// Prevent collision
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
